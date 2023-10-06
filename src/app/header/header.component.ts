329ef427e7ed8b5b61d033c1f9fd13c4e6726cff
Abhishek-7017 <abhishekat7500@gmail.com>
2023-10-07 00:27:03 +0530

    seller home navigation changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,22 +7,32 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  menuType:string='default';
+  menuType: string = 'default';
+  sellerName: string = '';
 
-  constructor(private route:Router){}
-  
+  constructor(private route: Router) { }
+
   ngOnInit(): void {
     this.route.events.subscribe(
-      (val:any)=>{
-        if(val.url){
-          if(localStorage.getItem('seller') && val.url.includes('seller')){
+      (val: any) => {
+        if (val.url) {
+          if (localStorage.getItem('seller') && val.url.includes('seller')) {
             this.menuType = 'seller';
-          }else{
+            if(localStorage.getItem('seller')){
+              let sellerStore = localStorage.getItem('seller');
+              let sellerData = sellerStore && JSON.parse(sellerStore)[0];
+              this.sellerName = sellerData.name;
+            }
+          } else {
             this.menuType = 'default';
           }
-          
+
         }
       })
   }
 
+  logout(){
+    localStorage.removeItem('seller');
+    this.route.navigate(['/']);
+  }
 }
