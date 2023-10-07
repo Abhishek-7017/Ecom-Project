6d293cef4125e2e9b902f3b7adf670d96032341a
Abhishek-7017 <abhishekat7500@gmail.com>
2023-10-07 23:13:24 +0530

    Updated Seller home, product list and update them

--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Product } from '../data.type';
 
 @Component({
   selector: 'app-seller-add-product',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./seller-add-product.component.css']
 })
 export class SellerAddProductComponent {
+  addProductMessage:string|undefined;
+  constructor(private product:ProductService) { }
 
+  submit(data:Product){
+    this.product.addProduct(data).subscribe((result)=>{
+      console.warn(result);
+      if(result){
+        this.addProductMessage = 'The Product is successfully added';
+      }
+    })
+    setTimeout(()=>{
+      this.addProductMessage=undefined
+    },3000);
+  }
 }
--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Product } from '../data.type';
+import { faTrash,faEdit } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-seller-home',
   templateUrl: './seller-home.component.html',
   styleUrls: ['./seller-home.component.css']
 })
-export class SellerHomeComponent {
+export class SellerHomeComponent implements OnInit {
+
+  productMessage: undefined | string;
+  productList: undefined | Product[];
+  deleteIcon = faTrash
+  editIcon = faEdit
+  
+  constructor(private product: ProductService) { }
+
+  ngOnInit(): void {
+    this.List();
+  }
+  deleteItem(id: number) {
+    this.product.deleteItem(id).subscribe((result)=>{
+      if(result){
+        this.productMessage = 'Item deleted successfully';
+        this.List();
+      }
+    })
+    setTimeout(()=>{
+      this.productMessage = undefined
+    },3000)
+  }
+  List(){
+    this.product.productList().subscribe((result) => {
+      console.warn(result);
+      this.productList = result;
+    })
+  }
 
 }
